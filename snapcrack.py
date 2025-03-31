2f062ed875add6596102471d900e9280d5db6a60
johnr162357 <johnathanrange770@gmail.com>
2025-03-31 02:38:32 +0000

    Update snapcrack.py

--- snapcrack.py
+++ snapcrack.py
@@ -38,7 +38,7 @@ print("████████████████████████�
 print("█                       SNAPCHAT CRACKER BY A HOT TRANS GIRL                         █")
 print("██████████████████████████████████████████████████████████████████████████████████████")
 
-print(f"[░]Now attempting to crack: {targetusername}")
+print(f"[░]Now attempting to crack: {serynity244827}")
 
 # Calling class 'Snapchat' from pysnap.__init__
 proxy = ''
